3222935869b5bc9a43640853be9270c8756d9b87
Camilo Londoño <jclondono@elicogrp.com>
2024-07-30 18:24:10 -0500

    Add: querysets

--- industrial_insights/home/admin.py
+++ industrial_insights/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Measurement
 
 # Register your models here.
+class MeasurementAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Measurement._meta.fields]
+
+admin.site.register(Measurement, MeasurementAdmin)
--- industrial_insights/home/models.py
+++ industrial_insights/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Measurement(models.Model):
+    timestamp = models.DateTimeField(null=True, blank=True)
+    pressure = models.FloatField(null=True, blank=True)
+    temperature = models.FloatField(null=True, blank=True)
+    box_units = models.PositiveIntegerField(null=True, blank=True)
+    kwh = models.FloatField(null=True, blank=True)
+    flow = models.FloatField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.timestamp}, {self.pressure}, {self.temperature}, {self.box_units}, {self.kwh}, {self.flow}"
